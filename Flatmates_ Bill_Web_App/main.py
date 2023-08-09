d26da12b5205aee452099b4b384b420df259f9ea
Alamgir55 <alamgir555hossain@gmail.com>
2023-08-09 03:35:39 -0400

    'app-complete'

--- Flatmates_ Bill_Web_App/main.py
+++ Flatmates_ Bill_Web_App/main.py
@@ -14,6 +14,22 @@ class BillFormPage(MethodView):
     def get(self):
         bill_form = BillForm()
         return render_template('bill_form_page.html', billform=bill_form)
+    
+    def post(self):
+        billform = BillForm(request.form)
+        
+        the_bill = flat.Bill(float(billform.amount.data), billform.period.data)
+        flatmate1 = flat.Flatmates(billform.name1.data, float(billform.days_in_house1.data))
+        flatmate2 = flat.Flatmates(billform.name2.data, float(billform.days_in_house2.data))
+        
+        return render_template("bill_form_page.html",
+                               result=True,
+                               billform=billform, 
+                               name1=flatmate1.name,
+                               amount1=flatmate1.pays(the_bill, flatmate2),
+                               name2=flatmate2.name,
+                               amount2=flatmate2.pays(the_bill, flatmate1)) 
+        
 
 class ResultsPage(MethodView):
     def post(self):
@@ -27,7 +43,7 @@ class ResultsPage(MethodView):
                                name1=flatmate1.name,
                                amount1=flatmate1.pays(the_bill, flatmate2),
                                name2=flatmate2.name,
-                               amount2=flatmate2.pays(the_bill, flatmate1))
+                               amount2=flatmate2.pays(the_bill, flatmate1)) 
         
 
 class BillForm(Form): 
@@ -44,7 +60,7 @@ class BillForm(Form):
      
 
 app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
-app.add_url_rule('/bill', view_func=BillFormPage.as_view('bill_form_page'))
-app.add_url_rule('/results', view_func=ResultsPage.as_view('results_page'))
+app.add_url_rule('/bill_form_page', view_func=BillFormPage.as_view('bill_form_page'))
+# app.add_url_rule('/results', view_func=ResultsPage.as_view('results_page'))
 
 app.run(debug=True)
